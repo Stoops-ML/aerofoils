b9be29a5c31aaef74c2ff16d9e4506624888f5a2
danielstoops <danielstoops25@gmail.com>
2020-04-18 22:48:48 +0300

    included transform ToTensor to dataset

--- run_NN.py
+++ run_NN.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
 import matplotlib.pyplot as plt
 import os
 import numpy as np
@@ -10,12 +11,13 @@ import sys
 
 
 class AerofoilDataset(Dataset):
-    def __init__(self, root_dir):
+    def __init__(self, root_dir, transform=None):
         """data loading"""
         self.root_dir = Path(root_dir)
         self.aerofoils = [file for file in os.listdir(root_dir)
                           if re.search(r"(.csv)$", file)
                           if os.path.isfile(root_dir / file)]
+        self.transform = transform
 
     def __getitem__(self, item):
         """index into dataset"""
@@ -25,6 +27,9 @@ class AerofoilDataset(Dataset):
         # read data (saves memory by not reading data in __init__)
         coords = np.loadtxt(self.root_dir / self.aerofoils[item], dtype=np.float32)  # no header in file
         sample = {"aerofoil": self.aerofoils[item], "coordinates": coords}
+
+        if self.transform:
+            sample = self.transform(sample)
 
         return sample
 
@@ -40,7 +45,7 @@ def show_aerofoil(aerofoil, coordinates):
     plt.show()
 
 
-def show_aerofoil_batch(sample_batched):
+def show_aerofoil_batch(sample_batched, batch_num):
     """show plot of aerofoils for a batch of samples."""
     aerofoils_batch, coordinates_batch = sample_batched['aerofoil'], sample_batched['coordinates']
     batch_size = len(aerofoils_batch)
@@ -51,8 +56,15 @@ def show_aerofoil_batch(sample_batched):
         ax.plot(coords[:, 0], coords[:, 1], 'r-')
         ax.title.set_text(aerofoil)
 
-    plt.suptitle('Batch from dataloader')
+    plt.suptitle(f'Batch #{batch_num} from dataloader')
     plt.show()
+
+
+class ToTensor(object):
+    """convert ndarrays in sample to Tensors."""
+
+    def __call__(self, sample): return {'aerofoil': sample['aerofoil'],
+                                        'coordinates': torch.from_numpy(sample['coordinates'])}
 
 
 # file configuration
@@ -69,10 +81,13 @@ batch_size = 4
 learning_rate = 0.001
 
 # import dataset
-train_dataset = AerofoilDataset(path / 'data' / 'out')  # NO TRANSFORMS DONE ON DATASET
+train_dataset = AerofoilDataset(path / 'data' / 'out' / 'train', transform=transforms.Compose([ToTensor()]))
+# note: don't actually need to do the ToTensor transform because this is already done by the dataloader. It's needed for
+# images where you need to switch axes
+valid_dataset = AerofoilDataset(path / 'data' / 'out' / 'valid')
 # show_aerofoil(**train_dataset[0])
 
 # dataloader
 dataloader = DataLoader(train_dataset, batch_size=4, shuffle=True, num_workers=4)
-# for batch in dataloader:
-#     show_aerofoil_batch(batch)
+for i, batch in enumerate(dataloader):
+    show_aerofoil_batch(batch, i)
