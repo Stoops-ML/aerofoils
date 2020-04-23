d3a01286c26ec785a3b3add5c0f1eca7054e9864
danielstoops <danielstoops25@gmail.com>
2020-04-23 11:25:40 +0300

    -automatically open and close TensorBoard

--- run_CNN.py
+++ run_CNN.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
+import torchvision
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
 import os
@@ -14,24 +15,37 @@ import ErrorMetrics as metrics
 import ShowAerofoil as show
 import AerofoilDataset as AD
 import TitleSequence as Title
+from torch.utils.tensorboard import SummaryWriter
 import sys
 import math
+import subprocess
 
-
-# title sequence
-Title.print_title([" ", "Convolutional neural network", "Outputs: Max ClCd @ angle"])
+# output switches
+save_model = False
+print_activations = True
+run_tensorboard = True
 
 # file configuration
+time_of_run = datetime.datetime.now().strftime("D%d_%m_%Y_T%H_%M_%S")
 path = Path(__file__).parent
 train_dir = path / 'data' / 'out' / 'train'
+valid_dir = path / 'data' / 'out' / 'valid'
 test_dir = path / 'data' / 'out' / 'test'
-time_of_run = datetime.datetime.now().strftime("D%d_%m_%Y_T%H_%M_%S")
-save_model = False
-print_activations = False
+print_dir = path / 'print' / time_of_run
+writer = SummaryWriter(print_dir / 'runs')
+if run_tensorboard:
+    TB_process = subprocess.Popen(["tensorboard", f"--logdir={print_dir/'runs'}"],
+                                  stdout=open(os.devnull, 'w'), stderr=subprocess.STDOUT)
+
+# title sequence
+save_model_str = "MODEL WILL BE SAVED" if save_model else "Model will NOT be saved"
+run_tensorboard_str = "TENSORBOARD RUNNING" if run_tensorboard else "TensorBoard not running"
+Title.print_title([" ", "Convolutional neural network", "Outputs: Max ClCd @ angle", f"{save_model_str}",
+                   f"{run_tensorboard_str}", f"Print directory: {'print/' + time_of_run}"])
 
 # device configuration
 torch.manual_seed(0)
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 # hyper parameters
 hidden_layers = [300, 300, 300, 300, 300, 300]
@@ -42,16 +56,16 @@ learning_rate = 0.01  # TODO add learning rate finder
 
 # import datasets
 train_dataset = AD.AerofoilDataset(train_dir, transform=transforms.Compose([AD.ToTensor()]))
-test_dataset = AD.AerofoilDataset(test_dir, transform=transforms.Compose([AD.ToTensor()]))
+valid_dataset = AD.AerofoilDataset(valid_dir, transform=transforms.Compose([AD.ToTensor()]))
 num_channels, input_size, output_size = AD.AerofoilDataset.get_sizes(train_dataset)
 
 # dataloaders
 train_loader = DataLoader(dataset=train_dataset, batch_size=bs, shuffle=True, num_workers=4)
-test_loader = DataLoader(dataset=test_dataset, batch_size=bs, shuffle=False, num_workers=4)
+valid_loader = DataLoader(dataset=valid_dataset, batch_size=bs, shuffle=False, num_workers=4)
 
 # show aerofoils
-# show.show_aerofoil(**train_dataset[0])
-# for i, batch in enumerate(test_loader):
+# show.show_aerofoil(writer, tensorboard=True, **train_dataset[0])
+# for i, batch in enumerate(valid_loader):
 #     show.show_aerofoil_batch(i, **batch)
 
 
@@ -138,9 +152,15 @@ criterion_ClCd = nn.MSELoss()
 criterion_angle = nn.MSELoss()
 optimiser = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
+# structure of model
+for sample in train_loader:
+    writer.add_graph(model, sample["coordinates"].view(-1, 1, input_size).float())
+writer.close()
+
 # training loop
 model.train()  # needed?
-for epoch in tqdm(range(num_epochs)):
+running_loss = 0.
+for epoch in (range(num_epochs)):  # tqdm doesn't seem to work with nested loops with one progress bar
     for i, sample in enumerate(train_loader):
         # reshape input to column vector
         sample["coordinates"] = sample["coordinates"].view(-1, 1, input_size).to(device)
@@ -152,45 +172,57 @@ for epoch in tqdm(range(num_epochs)):
         loss_ClCd = criterion_ClCd(pred_ClCd, ClCd_batch)
         loss_angle = criterion_angle(pred_angle, angle_batch)
         loss = loss_ClCd + loss_angle
+        running_loss += loss.item()
 
         # backward pass
         optimiser.zero_grad()
         loss.backward()
         optimiser.step()
 
-        # print output
-        if (epoch+1) % 100 == 0 and (i+1) % len(train_loader) == 0:
-            with torch.no_grad():  # don't add gradients of test set to computational graph
-                ClCd = torch.tensor([])
-                angle = torch.tensor([])
-                predicted_ClCd = torch.tensor([])
-                predicted_angle = torch.tensor([])
-                for sample_batched in test_loader:
-                    sample_batched["coordinates"] = sample_batched["coordinates"].view(-1, 1, input_size).to(device)
-                    ClCd_batch = sample_batched["y"][:, 0].to(device)
-                    angle_batch = sample_batched["y"][:, 1].to(device)
+    # print output
+    if (epoch+1) % 100 == 0:
+        writer.add_scalar("training loss", running_loss / 1, epoch * len(train_loader) + i)  # /1 because 1 steps
+        running_loss = 0.
+        valid_loss = 0.
+        # TODO make better variable names here
+        with torch.no_grad():  # don't add gradients of test set to computational graph
+            ClCd = torch.tensor([])
+            angle = torch.tensor([])
+            predicted_ClCd = torch.tensor([])
+            predicted_angle = torch.tensor([])
+            for sample_batched in valid_loader:
+                sample_batched["coordinates"] = sample_batched["coordinates"].view(-1, 1, input_size).to(device)
+                ClCd_batch = sample_batched["y"][:, 0].to(device)
+                angle_batch = sample_batched["y"][:, 1].to(device)
 
-                    pred_ClCd, pred_angle = model(sample_batched["coordinates"].float())
-                    ClCd = torch.cat((ClCd_batch, ClCd), 0)
-                    angle = torch.cat((angle_batch, angle), 0)
-                    predicted_ClCd = torch.cat((pred_ClCd, predicted_ClCd), 0)
-                    predicted_angle = torch.cat((pred_angle, predicted_angle), 0)
+                pred_ClCd, pred_angle = model(sample_batched["coordinates"].float())
+                ClCd = torch.cat((ClCd_batch, ClCd), 0)
+                angle = torch.cat((angle_batch, angle), 0)
+                predicted_ClCd = torch.cat((pred_ClCd, predicted_ClCd), 0)
+                predicted_angle = torch.cat((pred_angle, predicted_angle), 0)
 
-            print(f"epoch {epoch+1}/{num_epochs}, step {i+1}/{len(train_loader)}.\n"
-                  f"Loss = {loss.item():.4f}\n"
-                  f"ClCd RMS: {metrics.root_mean_square(predicted_ClCd, ClCd):.2f}, "
-                  f"angle RMS: {metrics.root_mean_square(predicted_angle, angle):.2f}\n")
+                valid_loss += criterion_angle(pred_angle, angle_batch).item() + \
+                              criterion_ClCd(pred_ClCd, ClCd_batch).item()
+                writer.add_scalar("validation loss", valid_loss / 1, epoch * len(train_loader) + i)  # /1 is average over number of iterations (not epochs)
 
-# result = model.decode(model((test_dataset[0])["coordinates"]).view(-1, num_channels, input_size))
+        print(f"epoch {epoch+1}/{num_epochs}, step {i+1}/{len(train_loader)}.\n"
+              f"Validation loss = {loss.item():.4f}\n"
+              f"ClCd RMS: {metrics.root_mean_square(predicted_ClCd, ClCd):.2f}, "
+              f"angle RMS: {metrics.root_mean_square(predicted_angle, angle):.2f}\n")
+
+writer.close()
+# result = model.decode(model((valid_dataset[0])["coordinates"]).view(-1, num_channels, input_size))
+# draw resulting image (in link)
 
 # test set
+# note this is actually using the validation dataset
 model.eval()  # turn off batch normalisation and dropout
 with torch.no_grad():  # don't add gradients of test set to computational graph
     ClCd = torch.tensor([])
     angle = torch.tensor([])
     predicted_ClCd = torch.tensor([])
     predicted_angle = torch.tensor([])
-    for sample_batched in test_loader:
+    for sample_batched in valid_loader:
         sample_batched["coordinates"] = sample_batched["coordinates"].view(-1, 1, input_size).to(device)
         ClCd_batch = sample_batched["y"][:, 0].to(device)
         angle_batch = sample_batched["y"][:, 1].to(device)
@@ -201,7 +233,7 @@ with torch.no_grad():  # don't add gradients of test set to computational graph
         predicted_ClCd = torch.cat((pred_ClCd, predicted_ClCd), 0)
         predicted_angle = torch.cat((pred_angle, predicted_angle), 0)
 
-print("Results after training:\n"
+print("Test set results:\n"
       f"ClCd RMS: {metrics.root_mean_square(predicted_ClCd, ClCd):.2f}, "
       f"angle RMS: {metrics.root_mean_square(predicted_angle, angle):.2f}")
 
@@ -224,13 +256,22 @@ if print_activations:
             activation[name] = output.detach()
         return hook
 
-    model.conv2.register_forward_hook(get_activation('conv2'))
-    output = model((test_dataset[0])["coordinates"].view(1, num_channels, input_size).float().to(device))
+    # model.conv2.register_forward_hook(get_activation('conv2'))
+    model.convolutions[6].register_forward_hook(get_activation('conv6'))
+    sample = (valid_dataset[0])["coordinates"]
+    output = model(sample.view(1, num_channels, input_size).float().to(device))
 
-    act = activation['conv2'].squeeze()
+    act = activation['conv6'].squeeze()
     # fig, axarr = plt.subplots(act.size(0))
-    plt.plot((test_dataset[0])["coordinates"].view(-1, input_size).numpy()[0], 'r+')
-    for idx in range(act.size(0)):
-        # axarr[idx].plot(act[idx])
-        plt.plot(act[idx])
-    plt.show()
+
+    # for idx in range(act.size(0)):
+    # axarr[idx].plot(act[idx])
+    # plt.plot(act[idx])
+
+    # print aerofoil
+
+
+# kill TensorBoard
+if run_tensorboard:
+    input("\nPress Enter to kill TensorBoard...")
+    subprocess.Popen(["kill", "-9", f"{TB_process.pid}"])
